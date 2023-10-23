33f94865a0c0bbf702ace059f7442ebf5b988bf6
Alexandre Demers-Roberge <alderoberge@gmail.com>
2023-10-22 22:56:04 -0400

    Added comment to FishMMO-Utils Class1 to explain the purpose of the Utils project

--- FishMMO-Utils/Class1.cs
+++ FishMMO-Utils/Class1.cs
@@ -1,3 +1,5 @@
 ﻿namespace FishMMO_Utils
 {
+    // Building the Utils project copies the dependencies directly to your Plugins folder.
+    // Check the .csproj for FishMMO-Utils if you want to see the included dependencies.
 }
