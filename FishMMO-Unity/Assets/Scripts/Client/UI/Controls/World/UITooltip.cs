b0d298fba5019b298357c789c819008dc99b4cf7
jimdroberts <jim.d.roberts@gmail.com>
2024-11-15 00:22:56 -0800

    Update UITooltip.cs
    UI Tooltip will now offset based on if the mouse cursor is on the top or bottom half of the screen. This should ensure the tooltip window is always completely visible.

--- FishMMO-Unity/Assets/Scripts/Client/UI/Controls/World/UITooltip.cs
+++ FishMMO-Unity/Assets/Scripts/Client/UI/Controls/World/UITooltip.cs
@@ -20,7 +20,15 @@ namespace FishMMO.Client
 		{
 			if (text != null && background != null)
 			{
-				Vector3 offset = new Vector3(0.0f, -background.rect.height, 0.0f);
+				// Get the current mouse position
+				Vector3 mousePosition = Input.mousePosition;
+
+				// Get the screen height
+				float screenHeight = Screen.height;
+
+				float yOffset = (mousePosition.y > screenHeight / 2) ? -background.rect.height : 0.0f;
+
+				Vector3 offset = new Vector3(0.0f, yOffset, 0.0f);
 				transform.position = Input.mousePosition + offset;
 			}
 		}
@@ -31,7 +39,16 @@ namespace FishMMO.Client
 			if (this.text != null)
 			{
 				this.text.text = text;
-				Vector3 offset = new Vector3(0.0f, -background.rect.height, 0.0f);
+				
+				// Get the current mouse position
+				Vector3 mousePosition = Input.mousePosition;
+
+				// Get the screen height
+				float screenHeight = Screen.height;
+
+				float yOffset = (mousePosition.y > screenHeight / 2) ? -background.rect.height : 0.0f;
+
+				Vector3 offset = new Vector3(0.0f, yOffset, 0.0f);
 				transform.position = Input.mousePosition + offset;
 				Show();
 			}
