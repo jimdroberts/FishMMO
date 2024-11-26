207113b00548852c3f865680d40563c8aec81dcb
jimdroberts <jim.d.roberts@gmail.com>
2024-11-26 01:18:58 -0800

    Update AchievementSystem
    Achievements that reward items will now attempt to place item rewards in your inventory. If the inventory is full the item will be placed in the bank.

--- FishMMO-Unity/Assets/Scripts/Server/World/SceneServer/AchievementSystem.cs
+++ FishMMO-Unity/Assets/Scripts/Server/World/SceneServer/AchievementSystem.cs
@@ -1,6 +1,7 @@
 ﻿using FishNet.Transporting;
 using System.Collections.Generic;
 using FishMMO.Shared;
+using FishMMO.Server.DatabaseServices;
 
 namespace FishMMO.Server
 {
@@ -10,7 +11,7 @@ namespace FishMMO.Server
 		{
 			if (ServerManager != null)
 			{
-				IAchievementController.OnCompleteAchievement += HandleAchievementRewards;
+				IAchievementController.OnCompleteAchievement += IAchievementController_HandleAchievementRewards;
 			}
 			else
 			{
@@ -22,28 +23,47 @@ namespace FishMMO.Server
 		{
 			if (ServerManager != null)
 			{
-				IAchievementController.OnCompleteAchievement -= HandleAchievementRewards;
+				IAchievementController.OnCompleteAchievement -= IAchievementController_HandleAchievementRewards;
 			}
 		}
 
-		private void HandleAchievementRewards(ICharacter character, AchievementTemplate template, AchievementTier tier)
+		private void IAchievementController_HandleAchievementRewards(ICharacter character, AchievementTemplate template, AchievementTier tier)
 		{
-			if (character != null &&
-				character.TryGet(out InventoryController inventoryController))
+			if (character == null)
 			{
-				List<BaseItemTemplate> itemRewards = tier.ItemRewards;
-				if (itemRewards != null && itemRewards.Count > 0 && inventoryController.FreeSlots() >= itemRewards.Count)
+				return;
+			}
+
+			using var dbContext = Server.NpgsqlDbContextFactory.CreateDbContext();
+			if (dbContext == null)
+			{
+				return;
+			}
+
+			List<BaseItemTemplate> itemRewards = tier.ItemRewards;
+			if (itemRewards != null &&
+				itemRewards.Count > 0)
+			{
+				if (character.TryGet(out IInventoryController inventoryController) &&
+					inventoryController.FreeSlots() >= itemRewards.Count)
 				{
 					List<InventorySetItemBroadcast> modifiedItemBroadcasts = new List<InventorySetItemBroadcast>();
 
 					for (int i = 0; i < itemRewards.Count; ++i)
 					{
-						Item newItem = new Item(123, 0, itemRewards[i].ID, 1);
+						Item newItem = new Item(itemRewards[i], 1);
 
 						if (inventoryController.TryAddItem(newItem, out List<Item> modifiedItems))
 						{
 							foreach (Item item in modifiedItems)
 							{
+								if (item == null)
+								{
+									continue;
+								}
+								
+								CharacterInventoryService.SetSlot(dbContext, character.ID, item);
+
 								modifiedItemBroadcasts.Add(new InventorySetItemBroadcast()
 								{
 									InstanceID = newItem.ID,
@@ -59,6 +79,46 @@ namespace FishMMO.Server
 					{
 						IPlayerCharacter playerCharacter = character as IPlayerCharacter;
 						playerCharacter.Owner.Broadcast(new InventorySetMultipleItemsBroadcast()
+						{
+							Items = modifiedItemBroadcasts,
+						}, true, Channel.Reliable);
+					}
+				}
+				else if (character.TryGet(out IBankController bankController) &&
+						 bankController.FreeSlots() >= itemRewards.Count)
+				{
+					List<BankSetItemBroadcast> modifiedItemBroadcasts = new List<BankSetItemBroadcast>();
+
+					for (int i = 0; i < itemRewards.Count; ++i)
+					{
+						Item newItem = new Item(itemRewards[i], 1);
+
+						if (inventoryController.TryAddItem(newItem, out List<Item> modifiedItems))
+						{
+							foreach (Item item in modifiedItems)
+							{
+								if (item == null)
+								{
+									continue;
+								}
+
+								CharacterInventoryService.SetSlot(dbContext, character.ID, item);
+
+								modifiedItemBroadcasts.Add(new BankSetItemBroadcast()
+								{
+									InstanceID = newItem.ID,
+									TemplateID = newItem.Template.ID,
+									Slot = newItem.Slot,
+									Seed = newItem.IsGenerated ? newItem.Generator.Seed : 0,
+									StackSize = newItem.IsStackable ? newItem.Stackable.Amount : 0,
+								});
+							}
+						}
+					}
+					if (modifiedItemBroadcasts.Count > 0)
+					{
+						IPlayerCharacter playerCharacter = character as IPlayerCharacter;
+						playerCharacter.Owner.Broadcast(new BankSetMultipleItemsBroadcast()
 						{
 							Items = modifiedItemBroadcasts,
 						}, true, Channel.Reliable);
